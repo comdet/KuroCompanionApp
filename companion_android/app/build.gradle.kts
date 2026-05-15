bd92299d7f1450feeb4abebea6a99d6db3f3396f
Khomdet Phueadphut <listzone@hotmail.com>
2026-05-15 11:07:25 +0700

    Bump versionCode/versionName to 0.2.0 for first tagged release

--- companion_android/app/build.gradle.kts
+++ companion_android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.carcompanion.companion"
         minSdk = 31
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         // GitHub release coordinates for the assets-pack downloader.
         // - REPO_OWNER / REPO_NAME locate the public release feed
